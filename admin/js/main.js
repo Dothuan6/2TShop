e57c3395085882013618faaaa528b2ef19ff3c81
Dothuan6 <thuann6222@gmail.com>
2023-10-13 09:56:12 +0700

    JS_admin

---
+++ admin/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $('.sidebar').css({
+            'width':'70px',
+            'font-size':'25px'
+        });
+        $('.text-link').hide();
+        $('.close-btn').show();
+        $('.menu-btn').hide();
+    });
+
+    $('.close-btn').click(function(){
+    $('.sidebar').css({
+        'width':'300px',
+        'font-size':'15px'
+    });
+    $('.text-link').show();
+    $('.close-btn').hide();
+    $('.menu-btn').show();
+    });
+});
