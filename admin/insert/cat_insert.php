55c509812c02f866c3fb77f0a23c29bff30fe8b2
Dothuan6 <thuann6222@gmail.com>
2023-10-15 16:54:12 +0700

    cat_insert

--- admin/insert/cat_insert.php
+++ admin/insert/cat_insert.php
@@ -1,14 +1,29 @@
 <?php
 require_once '../../class/Crud.php';
 $obj = new Crud();
-
-if(empty($_POST["category_name"])){
-    echo "<script>alert('Nhập vào tên Danh Mục')</script>";
-}else{
-    $data = [
-        'category_name'=> $_POST['category_name']
-    ];
-    $result=$obj->insert('category',$data);
+//insert
+if($_POST['form_type'] == 'save'){
+    if(empty($_POST["category_name"])){
+        echo "<script>alert('Nhập vào tên Danh Mục')</script>";
+    }else{
+        $data = [
+            'category_name'=> $_POST['category_name']
+        ];
+        $result=$obj->insert('category',$data);
+    }
+    echo json_encode($data);
 }
-echo json_encode($data);
+//update
+if($_POST['form_type'] == 'edit'){
+    if(empty($_POST["category_name"])){
+        echo "<script>alert('Nhập vào tên Danh Mục')</script>";
+    }else{
+        $data = [
+            'category_name'=> $_POST['category_name']
+        ];
+        $cat_id = $_POST['cat_id'];
+        $result=$obj->update('category',$data,"where category_id = '$cat_id'");
+    }
+    echo json_encode($data);
+}
 ?>
