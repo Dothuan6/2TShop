ded01a5b498a84f8c08e00083e42392459e0bae1
Dothuan6 <thuann6222@gmail.com>
2023-10-15 16:53:46 +0700

    category

--- admin/category.php
+++ admin/category.php
@@ -2,6 +2,15 @@
 require_once 'includes/header.php';
 require_once '../class/Crud.php';
 $obj = new Crud(); 
+$no_of_records_per_page =2;
+
+if(isset($_GET['pageno'])){
+    $pageno =$_GET['pageno'];
+
+}else{
+    $pageno = 1;
+}
+$offset = ($pageno-1)*$no_of_records_per_page;
 
  ?>
 <div class="container">
@@ -9,7 +18,7 @@ $obj = new Crud();
     <section class="category-section">
         <h1 class="text-uppercase border-bottom shadow-5">Danh Mục</h1>
         <!-- Button trigger modal -->
-        <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" class="btn btn-info add_category" data-bs-toggle="modal" data-bs-target="#exampleModal">
             Thêm danh mục
         </button>
 
@@ -32,17 +41,16 @@ $obj = new Crud();
                             </div>
                     </div>
                     <div class="modal-footer">
-                        <input type="hidden" id="cat_id" name="id">
+                        <input type="hidden" id="cat_id" name="cat_id">
                         <input type="hidden" name="form_type" id="form_type">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
-                        <button type="submit" class="btn btn-primary save">Lưu</button>
+                        <button type="submit" class="btn btn-primary save" id="submit">Lưu</button>
                     </div>
                     </form>
 
                 </div>
             </div>
         </div>
-
         <table class="table table-bordered mt-3">
             <tr class="bg-info">
                 <th>ID</th>
@@ -52,19 +60,31 @@ $obj = new Crud();
             </tr>
             <?php 
             $num = 1;
-            foreach($obj->get() as $row){
+            foreach($obj->get('category',$offset,$no_of_records_per_page) as $row){
             ?>
-            <tr>
+            <tr id="category_<?php echo $row['category_id']; ?>">
                 <td><?php echo $num ++ ?></td>
                 <td><?php echo $row['category_name'] ?></td>
-                <td class="text-center w-25"><button class="btn btn-outline bg-warning"><i
+                <td class="text-center w-25"><button id="<?php echo $row['category_id']; ?>"
+                        class="btn btn-outline bg-warning edit" name="edit" type="button"><i
                             class="fa-solid fa-pen-to-square"></i></button></td>
-                <td class="text-center w-25"><button class="btn btn-outline bg-danger"><i
-                            class="fa-solid fa-trash text-white"></i></button>
+                <td class="text-center w-25"><a href="category.php?delete_id=<?php echo $row['category_id']; ?>"
+                        class="btn btn-outline btn-danger delete" name='delete'>
+                        <i class="fa-solid fa-trash text-white"></i>
+                    </a>
                 </td>
             </tr>
             <?php } ?>
         </table>
+        <ul class="pagination">
+            <?php
+                $total_pages = $obj->pagination('category',$no_of_records_per_page);
+                for($i=1;$i<=$total_pages;$i++){
+                    echo "<li class='page-item'><a href='?pageno=$i'class='page-link'> $i </a></li>";
+                } 
+         ?>
+
+        </ul>
 
     </section>
 </div>
@@ -93,5 +113,39 @@ $(document).ready(function() {
             }
         });
     });
+    $('.edit').click(function() {
+        var cat_id = $(this).attr('id');
+        var btn = "edit";
+        $('#exampleModal').modal('show');
+        $('.modal-title').text('Cập nhật Danh Mục');
+        $('#submit').removeClass('btn btn-primary btn-outline save').addClass(
+            'btn btn-warning btn-outline update').text('update');
+        // tồn tại edit thì hiện 'cập nhật' // không thì chạy qua bên main.js để xuất hiện 'lưu' 
+        $('#form_type').val('edit');
+        $.ajax({
+            url: "action/cat_action.php",
+            method: "POST",
+            data: {
+                cat_id: cat_id,
+                action: btn,
+            },
+            dataType: "json",
+            success: function(res) {
+                //truyền giá trị name vào input
+                $('#cat_id').val(res.category_id);
+                $('#category_name').val(res.category_name);
+            }
+        });
+
+    });
 });
 </script>
+<?php
+if(isset($_GET['delete_id'])){
+    $cat_id = $_GET['delete_id'];
+    $delete = $obj->delete('category',"where category_id = '$cat_id'");
+    if($delete){
+        echo "<script>alert('Xóa thành công!')</script>";
+    }
+}
+?>
